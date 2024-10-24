a1bc8803762b0d67102d1b680ad6b9d5c936391d
doyeonkim-cubox-ai <doyeon.kim@cubox.ai>
2024-10-24 16:39:26 +0900

    FEAT : add data normalization

--- yolov1/litdm.py
+++ yolov1/litdm.py
@@ -14,7 +14,8 @@ class PascalVOC(L.LightningDataModule):
         self.batch_size = batch_size
         self.transform = transforms.Compose([
             transforms.Resize((448, 448)),
-            transforms.ToTensor()
+            transforms.ToTensor(),
+            transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
         ])
 
     def prepare_data(self):
